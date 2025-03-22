ec07382f75aa1add4cc08aaecdf3ec76a800bca4
divyanshuyadav2 <divyanshu.yadav@bolgatty.nl>
2025-03-22 14:53:15 +0530

    new

---
+++ src/app/frontend/nav-bar/nav-bar.component.ts
@@ -0,0 +1,17 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-bar',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './nav-bar.component.html',
+  styleUrl: './nav-bar.component.css'
+})
+export class NavBarComponent {
+  isOpen = false;
+ companyEmail: string = 'wellbecure.wbc@gmail.com';
+  toggleMenu() {
+    this.isOpen = !this.isOpen;
+  }
+}

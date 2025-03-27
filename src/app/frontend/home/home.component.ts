b81284d8d3034c24f8de76f2a5cdad2cc3774491
divyanshuyadav2 <divyanshu.yadav@bolgatty.nl>
2025-03-27 17:48:17 +0530

    fix2

--- src/app/frontend/home/home.component.ts
+++ src/app/frontend/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { NavBarComponent } from "../nav-bar/nav-bar.component";
 import { JsonLdService } from '../../service/json-ld.service';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavBarComponent],
+  imports: [NavBarComponent,RouterModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
--- src/app/frontend/nav-bar/nav-bar.component.ts
+++ src/app/frontend/nav-bar/nav-bar.component.ts
@@ -4,7 +4,7 @@ import { Router, RouterModule } from '@angular/router';
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,RouterModule],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.css'
 })
